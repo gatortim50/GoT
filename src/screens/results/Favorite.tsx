1d35457ec00aab13f8b2cf323f9e911c17ab48a3
Tim McKee <timothy.mckee@momentumdesignlab.com>
2022-03-09 08:40:36 -0500

    favs

--- src/screens/results/Favorite.tsx
+++ src/screens/results/Favorite.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, TouchableOpacity } from 'react-native';
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
 import { faHeart } from '@fortawesome/free-solid-svg-icons';
@@ -8,23 +8,34 @@ import ResultsScreen from './Results';
 
 interface IProps {
   title: string;
-    itemId: number;
+  itemId: number;
 }
 
 const Favorite = ({ title, itemId }: IProps) => {
   const [fav, setFav] = useState(false);
-  let favArray = new Array<string>(10);
+  const [favArray, setFavArray] = useState([]);
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    console.log(`change to ${counter}`);
+  }, [counter]);
+
+  let LEN = 0;
+  const MAX = 10;
   let icon = true;
 
   const onFav = () => {
     setFav(!fav);
-    if (favArray.includes(title)) {
-      favArray.pop(title);
-    } else {
-      favArray.push(title);
-    }
     console.log(title);
-    console.log(favArray);
+    if (favArray.includes(title)) {
+      console.log('TRUE contains:', title);
+      setCounter(counter - 1);
+    } else {
+      console.log('FALSE add:', title);
+      setFavArray(title);
+      setCounter(counter + 1);
+    }
+    // console.log(counter);
   };
 
   return (
